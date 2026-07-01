34eb8e859bfba5fb4523cd5a71a1d73160f95fd7
KyLeo25 <vertusarmageddon88@gmail.com>
2026-07-01 22:39:34 +0700

    Perubahan sql

--- backend/src/config/mysql.js
+++ backend/src/config/mysql.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 3307,
     dialect: "mysql",
     logging: false,
     pool: {
